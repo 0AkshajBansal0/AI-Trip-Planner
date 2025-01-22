bb035547a36e3a0cec045e114706624b13e2677d
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-22 10:46:41 +0530

    .

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '../ui/button';
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
+      <img 
+        src="/TripPlanner.png" 
+        alt="Logo" 
+        style={{ height: '50px', width: 'auto' }} 
+      />
+      <div>
+        <Button>Sign In</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,14 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-full w-full">
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+      </div>
+      <div>
+        <Header/>
+      </div>
+    </div>
     <RouterProvider router={router} />
   </StrictMode>,
 )
