c529a06e3f7116720369f9a36084900ab7645eef
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-25 16:11:54 +0530

    aouth

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/header.jsx'
 import { Toaster } from './components/ui/sonner'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const router = createBrowserRouter([
   {
@@ -20,15 +21,17 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full">
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+        <div className="fixed top-0 -z-10 h-full w-full">
+        <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+        </div>
+        <div>
+          <Header/>
+          <Toaster/>
+        </div>
       </div>
-      <div>
-        <Header/>
-        <Toaster/>
-      </div>
-    </div>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
