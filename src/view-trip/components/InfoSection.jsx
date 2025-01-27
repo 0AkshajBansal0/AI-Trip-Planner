e499decc41a7179411b2d5ac197ff777799891f9
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-27 23:14:27 +0530

    changes

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -11,9 +11,9 @@ const InfoSection = ({ trip }) => {
         <div className='my-5 flex flex-col gap-2'>
           <h2 className='font-bold text-2xl'>{trip?.userSelection?.location?.label}</h2>
           <div className='flex gap-5'>
-            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>🗓️ {trip?.userSelection?.noOfDays} Days</h2>
-            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>💰 {trip?.userSelection?.budget} Budget</h2>
-            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>🥂 No. Of Travelers: {trip?.userSelection?.traveler}</h2>
+            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>🗓️ {trip?.userSelection?.noOfDays} Days</h2>
+            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>💰 {trip?.userSelection?.budget} Budget</h2>
+            <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>🥂 No. Of Travelers: {trip?.userSelection?.traveler}</h2>
           </div>
         </div>
         <Button><IoIosSend /></Button>
