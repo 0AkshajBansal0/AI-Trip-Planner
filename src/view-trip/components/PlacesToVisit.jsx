119f0dce9dca66692c8b28202f9000a96b3bf924
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-29 22:17:50 +0530

    places section

--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -7,7 +7,7 @@ const PlacesToVisit = ({ trip }) => {
       <h2 className="font-bold text-lg">Places To Visit</h2>
 
       <div>
-        {trip?.tripData?.itinerary?.map((item, index) => (
+        {trip?.tripData?.dailyItinerary?.map((item, index) => (
           <div className='mt-5'>
             <h2 className='font-medium text-lg'>{item.day}</h2>
             <div className='grid md:grid-cols-2 gap-5'>
