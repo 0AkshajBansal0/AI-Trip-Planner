db7c671b358d598f71e0a7335ddcaa497270ffa4
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-29 22:18:11 +0530

    added image portion

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { IoIosSend } from "react-icons/io";
+import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalAPI';
 
-const InfoSection = ({ trip }) => {
+function InfoSection({ trip }) {
+  
+  const [photoUrl,setPhotoUrl]=useState();
+  useEffect(()=>{
+    trip&&GetPlacePhoto();
+  }, [trip])
+  const GetPlacePhoto=async()=>{
+    const data={
+      textQuery: trip?.userSelection?.location?.label
+    }
+    const result = await GetPlaceDetails(data).then(resp=>{
+      console.log(resp.data.places[0].photos[3].name)
+
+      const PhotoUrl=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name)
+      setPhotoUrl(PhotoUrl);
+    })
+  }
+
   return (
     <div>
-      <img src="/placeholder.webp" className='h-[340px] w-full object-cover rounded-xl' />
+      <img src={photoUrl?photoUrl:"/placeholder.webp"} className='h-[340px] w-full object-cover rounded-xl' />
 
       <div className='flex justify-between items-center'>
         <div className='my-5 flex flex-col gap-2'>
