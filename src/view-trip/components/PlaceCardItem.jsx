1bfe063e7d3790e0f7f93c888e6b84960d0630b1
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-30 09:28:25 +0530

    changed styling, made responsive

--- src/view-trip/components/PlaceCardItem.jsx
+++ src/view-trip/components/PlaceCardItem.jsx
@@ -1,40 +1,39 @@
 import React, { useEffect, useState } from 'react'
-import { Button } from '@/components/ui/button'
 import { FaMapLocationDot } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalAPI';
 
-const PlaceCardItem = ({place}) => {
+const PlaceCardItem = ({ place }) => {
 
-  const [photoUrl,setPhotoUrl]=useState();
-    useEffect(()=>{
-      place&&GetPlacePhoto();
-    }, [place])
-    const GetPlacePhoto=async()=>{
-      const data={
-        textQuery: place?.placeName
-      }
-      const result = await GetPlaceDetails(data).then(resp=>{
-        //console.log(resp.data.places[0].photos[3].name)
+  const [photoUrl, setPhotoUrl] = useState();
   
-        const PhotoUrl=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name)
-        setPhotoUrl(PhotoUrl);
-      })
+  useEffect(() => {
+    place && GetPlacePhoto();
+  }, [place]);
+
+  const GetPlacePhoto = async () => {
+    const data = {
+      textQuery: place?.placeName
     }
+    const result = await GetPlaceDetails(data).then(resp => {
+      const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[3].name);
+      setPhotoUrl(PhotoUrl);
+    })
+  }
 
   return (
     <Link to={"https://www.google.com/maps/search/?api=1&query=" + place?.placeName + ", "} target="_blank">
-    <div className='border rounded-xl p-3 mt-2 flex gap-5 hover:scale-105 transition-all hover:shadow-md cursor-pointer'>
-        <img src={photoUrl?photoUrl:"/placeholder.webp"}
-        className='w-[130px] h-[130px] rounded-xl object-cover'
+      <div className='border rounded-xl p-3 mt-2 flex gap-5 transition-all hover:shadow-lg hover:scale-105 cursor-pointer transform duration-300'>
+        <img 
+          src={photoUrl ? photoUrl : "/placeholder.webp"}
+          className='w-[130px] h-[130px] rounded-xl object-cover transition-all transform duration-300 hover:scale-105'
         />
-        <div>
-            <h2 className='font-bold text-lg'>{place.placeName}</h2>
-            <p className='text-sm text-gray-400'>{place.placeDetails}</p>
-            <h2 className='mt-2'>🕙 {place.timeToTravel}</h2>
-            {/*<Button size="sm"><FaMapLocationDot /></Button>*/}
+        <div className="flex flex-col justify-between">
+          <h2 className='font-bold text-lg text-gray-900'>{place.placeName}</h2>
+          <p className='text-sm text-gray-500'>{place.placeDetails}</p>
+          <h2 className='mt-2 text-sm text-gray-600'>🕙 {place.timeToTravel}</h2>
         </div>
-    </div>
+      </div>
     </Link>
   )
 }
