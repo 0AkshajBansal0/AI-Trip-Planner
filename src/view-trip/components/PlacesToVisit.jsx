f71e5b79b892658707549573a8019dede806e661
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-31 09:32:18 +0530

    minor changes

--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -7,7 +7,7 @@ const PlacesToVisit = ({ trip }) => {
       <h2 className="font-bold text-lg sm:text-xl md:text-2xl mb-6">Places To Visit</h2>
 
       <div>
-        {trip?.tripData?.dailyItinerary?.map((item, index) => (
+        {trip?.tripData?.itinerary?.map((item, index) => (
           <div key={index} className="mt-8">
             <h2 className="font-medium text-lg sm:text-xl text-gray-800">{item.day}</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-4">
